33f05cce99cfed6cc89be0fbf4819c9835c6051d
BohraDJayesh <jayeshduttbohra@gmail.com>
2023-12-19 18:47:37 +0530

    Function to print lines accross the box has been written and a SARV at the top, almost written function to to check or open a file and return a handle to it, then printing the values inside the box then will go for the scrolling part.

--- Sarv.cpp
+++ Sarv.cpp
@@ -1,20 +1,107 @@
-// Sarv.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
+#include <cctype>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#define WINDOWS_IGNORE_PACKING_MISMATCH
+SHORT consoleLength;
+SHORT consoleWidth;
+
+ void *consoleDim( CONSOLE_SCREEN_BUFFER_INFO csbi) {
+    consoleLength = (csbi.srWindow.Right - csbi.srWindow.Left + 1);
+    consoleWidth = (csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Function to print boundaries and the name "Surv" at top.
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void printBoundaries(HANDLE hConsole,CONSOLE_SCREEN_BUFFER_INFO csbi) {
+
+    //Calculating center of the console or terminal.
+
+    short centerX = ((csbi.dwSize.X) / 2);
+    short centerY = ((csbi.dwSize.Y) / 2);
+
+    for (int i = 1; i < centerX-2; i++) {
+        if (i == centerX - 2)
+            std::cout << "SARV" << std::endl;
+    }
+    
+    //Now printing the whole box.
+
+    for (int i = 0; i < consoleLength-1; i++)
+    {
+        for (int j = 0; j < consoleWidth; j++) {
+            if (i != 0 && i != consoleLength - 1) {
+                if ((i == 1 || i == consoleLength - 2))
+                    std::cout << "-";
+                else if (j == 1 || j == consoleWidth - 1)
+                    std::cout << "|";
+                else
+                    std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+
+
+}
+
+
+// Function to load a file and return it's handle/pointer.
+
+HANDLE OpenFile(const wchar_t* filePath) {
+    HANDLE hFile = CreateFile(filePath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        DWORD error = GetLastError();
+        std::cout << "Error opening file. Error code: " << error << std::endl;
+        return NULL;
+    }
+    return hFile;
+}
+
+//Checkinf if the file exists or not. !!
+
+bool FileOrDirectoryExists(const char* filePath) {
+    DWORD attributes = GetFileAttributesA(filePath);
+    return (attributes != INVALID_FILE_ATTRIBUTES);
+}
+
+
+// Function to read file 
+
+
+int main(int argc, char* argv[]) {
+
+
+    //Getting the information of cosole :
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi; // Stores information regarding console window, like size, width, height etc.
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+
+
+
+    // Calling printBoundaries function to print the boundaries accross the terminal.
+
+    printBoundaries(hConsole, csbi);
+
+    //Reading the file, checking if it's exists or not, creating one.
+
+    const char* narrowString = argv[1];
+    int wideStringLength = MultiByteToWideChar(CP_UTF8, 0, narrowString, -1, NULL, 0);
+    wchar_t* filePath = new wchar_t[wideStringLength];
+    MultiByteToWideChar(CP_UTF8, 0, narrowString, -1, filePath, wideStringLength);
+
+
+
+    if(FileOrDirectoryExists(argv[1])) {
+        std::cout<<"File Doesn't Exists.."<<std::endl;
+        std::cout<<"Press Enter to create one !!"<<std::endl;
+
+        if(getchar())
+            OpenFile(filePath);
+
+
+    }
+}
+
