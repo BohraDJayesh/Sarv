97f6ec223d4e96e0fca3095adce30cbbb63efbf6
BohraDJayesh <jayeshduttbohra@gmail.com>
2024-01-22 21:59:45 +0530

    Relocated cursor at the beginning, made the function for clearscreen when pressed enter, implemented multi-threading to continously monitor for ctrl+c event, so upon quitting it can clear the screen by itself.

--- Sarv.cpp
+++ Sarv.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <Windows.h>
 #include <cctype>
+#include <thread>
 
 #define WINDOWS_IGNORE_PACKING_MISMATCH
-SHORT consoleLength;
-SHORT consoleWidth;
+LONG consoleLength;
+LONG consoleWidth;
 
- void *consoleDim( CONSOLE_SCREEN_BUFFER_INFO csbi) {
-    consoleLength = (csbi.srWindow.Right - csbi.srWindow.Left + 1);
-    consoleWidth = (csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
+ void consoleDim( CONSOLE_SCREEN_BUFFER_INFO csbi) {
+    //consoleLength = (csbi.srWindow.Right - csbi.srWindow.Left + 1);
+     consoleLength = csbi.dwSize.X;
+     consoleWidth = csbi.dwSize.Y;
+    //consoleWidth = (csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
 }
 
 // Function to print boundaries and the name "Surv" at top.
@@ -20,24 +23,29 @@ void printBoundaries(HANDLE hConsole,CONSOLE_SCREEN_BUFFER_INFO csbi) {
     short centerX = ((csbi.dwSize.X) / 2);
     short centerY = ((csbi.dwSize.Y) / 2);
 
-    for (int i = 1; i < centerX-2; i++) {
+    for (int i = 1; i <= centerX-2; i++) {
         if (i == centerX - 2)
             std::cout << "SARV" << std::endl;
+        else
+        {
+            std::cout << " ";
+        }
     }
     
     //Now printing the whole box.
 
-    for (int i = 0; i < consoleLength-1; i++)
+    for (int i = 0; i < 2*centerY - 1; i++)
     {
-        for (int j = 0; j < consoleWidth; j++) {
-            if (i != 0 && i != consoleLength - 1) {
-                if ((i == 1 || i == consoleLength - 2))
-                    std::cout << "-";
-                else if (j == 1 || j == consoleWidth - 1)
-                    std::cout << "|";
-                else
-                    std::cout << " ";
-            }
+        if (i == 0) //to skip the first line for SARV test.
+            continue;
+        for (int j = 0; j < 2*centerX; j++) {
+
+            if ((i == 1 || i == 2*centerY - 2))
+                std::cout << "-";
+            else if (j == 0 || j == 2*centerX - 1)
+                std::cout << "|";
+            else
+                std::cout << " ";
         }
         std::cout << std::endl;
     }
@@ -46,62 +54,106 @@ void printBoundaries(HANDLE hConsole,CONSOLE_SCREEN_BUFFER_INFO csbi) {
 
 }
 
+//function to clear the screen.
+#include <iostream>
+#include <Windows.h>
 
-// Function to load a file and return it's handle/pointer.
+void ClearScreen() {
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-HANDLE OpenFile(const wchar_t* filePath) {
-    HANDLE hFile = CreateFile(filePath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        DWORD error = GetLastError();
-        std::cout << "Error opening file. Error code: " << error << std::endl;
-        return NULL;
+    if (hStdOut == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error getting console handle: " << GetLastError() << std::endl;
+        return;
     }
-    return hFile;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hStdOut, &dwMode)) {
+        std::cerr << "Error getting console mode: " << GetLastError() << std::endl;
+        return;
+    }
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+    if (!SetConsoleMode(hStdOut, dwMode)) {
+        std::cerr << "Error setting console mode: " << GetLastError() << std::endl;
+        return;
+    }
+
+    // ANSI escape sequence for clearing the screen
+    const char* clearScreenSequence = "\x1b[2J";
+
+    DWORD dwWritten = 0;
+    if (!WriteConsoleA(hStdOut, clearScreenSequence, static_cast<DWORD>(strlen(clearScreenSequence)), &dwWritten, nullptr)) {
+        std::cerr << "Error writing to console: " << GetLastError() << std::endl;
+    }
+
+    // Restore the original console mode
+    SetConsoleMode(hStdOut, dwMode);
 }
 
-//Checkinf if the file exists or not. !!
 
-bool FileOrDirectoryExists(const char* filePath) {
-    DWORD attributes = GetFileAttributesA(filePath);
-    return (attributes != INVALID_FILE_ATTRIBUTES);
+
+std::atomic<bool> exitRequested(false);
+
+// Function to handle Ctrl+C event
+BOOL CtrlHandler(DWORD fdwCtrlType) {
+    switch (fdwCtrlType) {
+    case CTRL_C_EVENT:
+        // Perform your task here
+        ClearScreen();
+        return TRUE; // Returning TRUE to prevent the default handler
+    default:
+        return FALSE;
+    }
 }
 
 
-// Function to read file 
+
+//function to relocate the pointer to the starting of the terminal.
+void RelocateCursor(HANDLE hConsole, int X, int Y) {
+    COORD newPosition = { static_cast<SHORT>(X), static_cast<SHORT>(Y) };
+    SetConsoleCursorPosition(hConsole, newPosition);
+}
+
+void ctrlMonitor() {
+
+}
+
+
 
 
 int main(int argc, char* argv[]) {
 
+    // Thread to continously monitor for ctrl+c event.
+
+    SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
+    std::thread monitorEvent(ctrlMonitor);
 
     //Getting the information of cosole :
 
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO csbi; // Stores information regarding console window, like size, width, height etc.
     GetConsoleScreenBufferInfo(hConsole, &csbi);
-
+    consoleDim(csbi);
 
 
     // Calling printBoundaries function to print the boundaries accross the terminal.
 
     printBoundaries(hConsole, csbi);
 
-    //Reading the file, checking if it's exists or not, creating one.
+    //Relocating the cursor to the starting position of the window.
+    RelocateCursor(hConsole,2,2);
 
-    const char* narrowString = argv[1];
-    int wideStringLength = MultiByteToWideChar(CP_UTF8, 0, narrowString, -1, NULL, 0);
-    wchar_t* filePath = new wchar_t[wideStringLength];
-    MultiByteToWideChar(CP_UTF8, 0, narrowString, -1, filePath, wideStringLength);
+    //More Code goes here, make your changes here rest all should be remained unchanged.
 
 
 
-    if(FileOrDirectoryExists(argv[1])) {
-        std::cout<<"File Doesn't Exists.."<<std::endl;
-        std::cout<<"Press Enter to create one !!"<<std::endl;
 
-        if(getchar())
-            OpenFile(filePath);
+    char temp = getchar();
 
+    monitorEvent.join();
+    ClearScreen();
+    
 
-    }
 }
 
